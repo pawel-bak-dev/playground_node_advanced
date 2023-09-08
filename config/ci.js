7fd4de1df02397b3bd4c77e39f5be945385267af
pawel-bak-dev <pawel.bak.dev@gmail.com>
2023-09-08 16:41:16 +0200

    ci | ci adjusting

--- config/ci.js
+++ config/ci.js
@@ -2,8 +2,7 @@ module.exports = {
   googleClientID:
     '908719155720-jdvudbjbve3juvmia3pc11p56g3n6ovm.apps.googleusercontent.com',
   googleClientSecret: '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',
-  mongoURI:
-    'mongodb+srv://pawelbakdev:Cc6Sfvs7JYDTn6oM@dev.fo6cqp8.mongodb.net/advnode?retryWrites=true&w=majority',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };
